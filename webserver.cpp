3df3bb96154324f4830993ab3e7885954e6a15bc
josh <jtlsc7@gmail.com>
2023-08-24 03:10:16 -0400

    listener up

--- webserver.cpp
+++ webserver.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <unistd.h>
 
 // https://www.boost.org/doc/libs/1_76_0/doc/html/boost_asio/overview/core/basics.html
 // https://www.bogotobogo.com/cplusplus/sockets_server_client.php
@@ -7,10 +8,16 @@ int main() {
 
     std::cout << "HI" << std::endl;
 
-    boost::asio::io_context io_context;
-    boost::asio::ip::tcp::socket socket(io_context);
+    boost::asio::io_service ios;
+    boost::asio::ip::tcp::acceptor acceptor(ios); 
+    boost::asio::ip::tcp::endpoint local_end(boost::asio::ip::address::from_string("127.0.0.1"), 80);
     boost::system::error_code ec;
-    socket.connect(server_endpoint, ec);
-    std::cout << ec << std::endl;
+    acceptor.open(local_end.protocol());
+    acceptor.bind(local_end, ec);
+    if (ec) std::cout << ec << std::endl;
+    else std::cout << "worked" << std::endl;
+    acceptor.listen(30);
+    boost::asio::ip::tcp::socket sock(ios);
+    acceptor.accept(sock);
     return 0;
 }
